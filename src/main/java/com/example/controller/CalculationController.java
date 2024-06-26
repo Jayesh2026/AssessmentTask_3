971935339bb30400c46c5dcb24334f136bd4eeb9
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-06-27 02:18:31 +0530

    API for execute task

--- src/main/java/com/example/controller/CalculationController.java
+++ src/main/java/com/example/controller/CalculationController.java
@@ -1,10 +1,22 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.CalculationService;
 
 @RestController
 @RequestMapping("/calculation")
 public class CalculationController {
+
+    @Autowired
+    private CalculationService calculationService;
+
+    @GetMapping("/run")
+    public void getRunAll() {
+        calculationService.executeTasks();
+    }
     
 }
